2839c168a1f9f1ac92dcc897337205e9106e792f
Sam-uvas <125831323+Sam-uvas@users.noreply.github.com>
2024-07-20 14:31:39 +0200

    Update task_model.dart

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,33 +1,5 @@
-/*class Task {
-  String title;
-  String description;
-  DateTime dateCreated;
-  bool isCompleted;
-
-  Task({
-    required this.title,
-    required this.description,
-    required this.dateCreated,
-    this.isCompleted = false, required DateTime createdAt,
-  });
-}
-*//*
 class Task {
-  String title;
-  String description;
-  DateTime createdAt;
-  bool isCompleted;
-
-  Task({
-    required this.title,
-    required this.description,
-    required this.createdAt,
-    this.isCompleted = false, required String id, 
-  });
-}
-*/
-class Task {
-  final String id; // Unique identifier for the task
+  final String id; 
   late final String title;
   late final String description;
   final DateTime createdAt;
